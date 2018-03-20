d24983fa4f0657f7a2683d528b0d974b671f94e0
Boka <BokaWT@corning.com>
2018-03-20 16:18:23 -0400

    Add Basic Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "wtb", password: "password", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
